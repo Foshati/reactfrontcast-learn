8df5da2d95a474a1cb1380b0492ebfe21c2a2640
Foshati <amirrezafoshati@gmail.com>
2024-09-29 21:41:54 +0330

    learn props children

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import UserList from "./components/UserList";
 
 export default function App() {
   return (
-    <>
-      <UserList   userName="ali" email="ali@gmail.com" />
-      <UserList   userName="reza" email="reza@gmail.com" />
-    </>
+    <main className="flex flex-col justify-center items-center  gap-10 p-4 m-4">
+      <UserList  userName="ali" email="ali@gmail.com" children/>
+      <UserList userName="reza" email="reza@gmail.com" children/>
+      <UserList userName="sina" email="sina@gmail.com">
+        <p>props-children</p>
+      </UserList>
+    </main>
   );
 }
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,11 +1,24 @@
-export default function UserList({userName, email}:{userName:string, email:string}) {
+// import { type ReactNode } from "react";
+
+import { PropsWithChildren } from "react";
+
+// type UserListProps = {
+//   userName: string;
+//   email: string;
+//   children: ReactNode
+// };
+
+
+type UserListProps =PropsWithChildren<{userName:string, email:string}>
+
+export default function UserList({ userName, email , children}: UserListProps) {
   return (
     <div>
-        
-        <div>
-            <p>{userName}</p>
-            <p>{email}</p>
-        </div>
+      <div>
+        <p>{userName}</p>
+        <p>{email}</p>
+        <p>{children}</p>
+      </div>
     </div>
-  )
+  );
 }
