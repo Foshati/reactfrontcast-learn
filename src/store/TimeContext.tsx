c73648892aaabe92f6421c163fae8f54dc828151
Foshati <amirrezafoshati@gmail.com>
2024-10-10 23:38:00 +0330

    create context  TimersContext

--- src/store/TimeContext.tsx
+++ src/store/TimeContext.tsx
@@ -1,5 +1,19 @@
-export default function TimeContext() {
-  return (
-    <div>TimeContext</div>
-  )
-}
+import { createContext } from "react";
+
+type Timer = {
+  name: string;
+  duration: Number;
+};
+
+type TimerState = {
+  isRunning: boolean;
+  timers: Timer[];
+};
+
+type TimersContextValue = TimerState & {
+  addTimers: (TimerData: Timer) => void;
+  startTimers: () => void;
+  stopTimers: () => void;
+};
+
+const TimersContext = createContext<TimersContextValue>(null);
