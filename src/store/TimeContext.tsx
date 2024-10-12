cd39f006c6827ef8ca97f7ab8bd8b6ca588ae7b6
Foshati <amirrezafoshati@gmail.com>
2024-10-12 23:48:08 +0330

    fix TimersContext

--- src/store/TimeContext.tsx
+++ src/store/TimeContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode } from "react";
 
 type Timer = {
   name: string;
@@ -22,36 +22,18 @@ type TimersContextProviderProps = {
 
 export const TimersContext = createContext<TimersContextValue | null>(null);
 
-
-
 export default function TimersContextProvider({
   children,
 }: TimersContextProviderProps) {
-  const [isRunning, setIsRunning] = useState(false);
-  const [timers, setTimers] = useState<Timer[]>([]);
-
-  const addTimers = (timerData: Timer) => {
-    setTimers((prevTimers) => [...prevTimers, timerData]);
+  const TimersContextValu: TimersContextValue = {
+    isRunning: false,
+    timers: [],
+    addTimers() {},
+    startTimers() {},
+    stopTimers() {},
   };
-
-  const startTimers = () => {
-    setIsRunning(true);
-  };
-
-  const stopTimers = () => {
-    setIsRunning(false);
-  };
-
-  const TimersContextValue: TimersContextValue = {
-    isRunning,
-    timers,
-    addTimers,
-    startTimers,
-    stopTimers,
-  };
-
   return (
-    <TimersContext.Provider value={TimersContextValue}>
+    <TimersContext.Provider value={TimersContextValu}>
       {children}
     </TimersContext.Provider>
   );
